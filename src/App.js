f76aa73dfcf7e19ca78626fc9e3190f0b7f110a9
Chance Harmon <henrikillsit@gmail.com>
2021-12-06 14:15:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>PCEP PREP</h2>
+      {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/vVjvXGjghgw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+      <script type="text/javascript" src="//cdn.datacamp.com/dcl/lastest/dcl-react.js.gz"></script>
+
+      <div data-datacamp-exercise data-lang="python">
+        <code data-type="pre-exercise-code">
+          # no pec
+        </code>
+        <code data-type="sample-code">
+          # Create a variable a, equal to 5
+
+
+          # Print out a
+
+        </code>
+        <code data-type="solution">
+          # Create a variable a, equal to 5
+          a = 5
+
+          # Print out a
+          print(a)
+        </code>
+        <code data-type="sct">
+          test_object("a")
+          test_function("print")
+          success_msg("Great job!")
+        </code>
+        <div data-type="hint">
+          Use the assignment operator (<code>=</code>) to create the variable <code>a</code>.
+        </div>
+      </div>
+
+      <iframe frameBorder="0" width="100%" height="500px" src="https://replit.com/@chanceTechSmart/test-embed?embed=true"></iframe>
+    </div>
+  );
+}
+
+export default App;
